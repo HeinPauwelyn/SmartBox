8423bbdd7bdc7c78dae6bc52ec17e2e2b95cba81
Hein Pauwelyn <hein.pauwelyn-vandewielle@student.howest.be>
2016-11-26 15:48:11 +0100

    fake data doorsturen en ontvangen

--- Execution/RaspberryPi/script.py
+++ Execution/RaspberryPi/script.py
@@ -1,1 +1,20 @@
-print ("hello world")
+import serial
+import json
+
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+counter = 0
+
+
+class Coordinates(object):
+
+    def __init__(self, j):
+        self.__dict__ = json.loads(j)
+
+while counter <= 100:
+    text = ser.readline()
+
+    coor = Coordinates(text)
+
+    print "lat: " + str(coor.lat)
+    print "long: " + str(coor.long)
+    counter += 1
