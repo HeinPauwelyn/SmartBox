3cdb3b0eebfa98b7f857481a93b534a1285b801e
Hein Pauwelyn <hein.pauwelyn-vandewielle@student.howest.be>
2016-12-02 18:24:16 +0100

    python

--- Execution/RaspberryPi/script.py
+++ Execution/RaspberryPi/script.py
@@ -8,6 +8,11 @@ class Coordinates(object):
 
     def __init__(self, j):
         self.__dict__ = json.loads(j)
+    
+    def decdeg2dms(dd):
+        mnt,sec = divmod(dd*3600,60)
+        deg,mnt = divmod(mnt,60)
+        return deg,mnt,sec
 
 while counter <= 100:
     text = ser.readline()
