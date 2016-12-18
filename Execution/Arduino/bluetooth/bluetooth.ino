7b9a4d2b8abef3a45591cb4d5919602651b0acf6
Hein Pauwelyn <hein.pauwelyn-vandewielle@student.howest.be>
2016-12-18 15:26:18 +0100

    bluetooth werkt

---
+++ Execution/Arduino/bluetooth/bluetooth.ino
@@ -0,0 +1,53 @@
+#include <SoftwareSerial.h>
+// int ledPin = 13;
+ 
+/* This sample code demonstrates the normal use of reading Bluetooth serial port.
+It requires the use of SoftwareSerial, and assumes that you have the 9600-baud Bluetooth serial device hooked up on pins 4(rx) and 3(tx).
+This way, port 0 and 1 stay clear for uploading and serial monitor.
+*/
+ 
+SoftwareSerial ss(20, 21);
+ 
+void setup() {
+  // Force the Bluetooth module to communicate 9600 baud on Serial
+  ss.begin(9600);
+  ss.print("$");
+  ss.print("$");
+  ss.print("$");
+  delay(100);
+  ss.println("SU,96");
+  delay(100);
+  ss.println("---");
+  ss.begin(9600);
+ 
+  // 9600 is the default baud rate for the screen module
+  Serial.begin( 9600 );
+  Serial.print("Start... ");
+}
+ 
+void loop() {
+  // listen for serial data
+  if ( ss.available() > 0 ) {
+    // read a numbers from serial port
+    int count = ss.parseInt();// print out the received number
+    if (count > 0) {
+      Serial.print("You have input: ");
+      Serial.println(String(count));
+ 
+      // blink the LED
+      // blinkLED(count);
+    }
+    else {
+      Serial.println("nope");
+    }
+  }
+}
+ 
+// void blinkLED(int count) {
+//   for (int i=0; i < count; i++) {
+//     digitalWrite(ledPin, HIGH);
+//     delay(500);
+//     digitalWrite(ledPin, LOW);
+//     delay(500);
+//   }
+// }
---
+++ Execution/SmartBox/SmartBox/MainPage.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth.Rfcomm;
+using Windows.Devices.Enumeration;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SmartBox
+{
+    public sealed partial class MainPage : Page
+    {
+        private StreamSocket _socket;
+        private RfcommDeviceService _service;
+        private DeviceInformationCollection _devices;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Loaded += MainPage_Loaded;
+        }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            btnDisconnect.IsEnabled = false;
+            _devices = await DeviceInformation.FindAllAsync(RfcommDeviceService.GetDeviceSelector(RfcommServiceId.SerialPort));
+
+            foreach (DeviceInformation d in _devices)
+            {
+                cboDevices.Items.Add(d.Name);
+            }
+        }
+
+        private async void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            int dummy;
+
+            if (!int.TryParse(tbInput.Text, out dummy))
+            {
+                tbError.Text = "Invalid input";
+            }
+
+            uint noOfCharsSent = await Send(tbInput.Text);
+
+            if (noOfCharsSent != 0)
+            {
+                tbError.Text = noOfCharsSent.ToString();
+            }
+        }
+        private async Task<uint> Send(string msg)
+        {
+            tbError.Text = string.Empty;
+
+            try
+            {
+                DataWriter writer = new DataWriter(_socket.OutputStream);
+
+                writer.WriteString(msg);
+
+                // Launch an async task to complete the write operation
+                return await writer.StoreAsync().AsTask();
+            }
+            catch (Exception ex)
+            {
+                tbError.Text = ex.Message;
+                return 0;
+            }
+        }
+
+        private async void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            await Connect();
+        }
+
+        private async void cboDevices_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            await Connect();
+        }
+
+        private async Task Connect()
+        {
+            tbError.Text = string.Empty;
+
+            try
+            {
+                DeviceInformation device = _devices.Single(x => x.Name == cboDevices.SelectedItem.ToString());
+
+                _service = await RfcommDeviceService.FromIdAsync(device.Id);
+                _socket = new StreamSocket();
+
+                await _socket.ConnectAsync(_service.ConnectionHostName, _service.ConnectionServiceName, SocketProtectionLevel.BluetoothEncryptionAllowNullAuthentication);
+                btnConnect.IsEnabled = false;
+                btnDisconnect.IsEnabled = true;
+                cboDevices.IsEnabled = false;
+            }
+            catch (Exception ex)
+            {
+                tbError.Text = ex.Message;
+            }
+        }
+
+        private async void btnDisconnect_Click(object sender, RoutedEventArgs e)
+        {
+            tbError.Text = string.Empty;
+
+            try
+            {
+                await _socket.CancelIOAsync();
+                _socket.Dispose();
+                _socket = null;
+                _service.Dispose();
+                _service = null;
+
+                btnDisconnect.IsEnabled = false;
+                btnConnect.IsEnabled = true;
+                cboDevices.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                tbError.Text = ex.Message;
+            }
+        }
+    }
+}
